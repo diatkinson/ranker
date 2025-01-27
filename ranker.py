73d44d8cfcef973e8fa8f45a013aac6dcf56d1b5
David Atkinson <davatk@gmail.com>
2025-01-26 19:17:52 -0500

    ruff; switch to boys

--- ranker.py
+++ ranker.py
@@ -1,27 +1,29 @@
-import time
 import sqlite3
+import time
 from sqlite3 import Connection
-import pymc as pm
-import numpy as np
-from pytensor.tensor.subtensor import set_subtensor
-import pytensor as pt
 
-K = 15 
+import numpy as np
+import pymc as pm
+import pytensor as pt
+from pytensor.tensor.subtensor import set_subtensor
+
+K = 15
+
 
 def init_db() -> Connection:
-    print('Initializing db...', end='')
-    conn = sqlite3.connect('names.db', isolation_level=None)
+    print("Initializing db...", end="")
+    conn = sqlite3.connect("names.db", isolation_level=None)
     cur = conn.cursor()
 
-    cur.execute('PRAGMA foreign_keys = ON')
+    cur.execute("PRAGMA foreign_keys = ON")
 
-    cur.execute('begin')
-    
-    cur.execute('create table if not exists names (name text primary key)')
-    if not cur.execute('select name from names').fetchone():
+    cur.execute("begin")
+
+    cur.execute("create table if not exists names (name text primary key)")
+    if not cur.execute("select name from names").fetchone():
         with open("names.txt") as f:
             names = f.read()
-        name_tuples = [(name,) for name in names.split('\n')]
+        name_tuples = [(name,) for name in set(names.split("\n"))]
         cur.executemany("insert into names (name) values (?)", name_tuples)
 
     cur.execute("""
@@ -42,24 +44,27 @@ def init_db() -> Connection:
         constraint fk_name foreign key (name) references names(name)
     )
     """)
-    
-    cur.execute('commit')
-    print(' done.')
+
+    cur.execute("commit")
+    print(" done.")
 
     return conn
-    
+
 
 def read_evaluations(conn: Connection) -> tuple[list[str], set[tuple[str, str]]]:
     cur = conn.cursor()
-    name_records = cur.execute('select name from names').fetchall()
-    names = [name_record for name_record, in name_records]
-    
-    evaluation_records = set(cur.execute('select winner, loser from evaluations').fetchall())
+    name_records = cur.execute("select name from names").fetchall()
+    names = [name_record for (name_record,) in name_records]
+
+    evaluation_records = set(cur.execute("select winner, loser from evaluations").fetchall())
 
     return names, evaluation_records
 
 
 def generate_estimates(names: list[str], evaluations: set[tuple[str, str]]) -> dict[str, tuple[float, float, float]]:
+    if not evaluations:
+        return {name: (0, 1, K / len(names)) for name in names}
+
     observed_data = np.zeros((len(names), len(names)))
     for winner, loser in evaluations:
         winner_idx = names.index(winner)
@@ -79,7 +84,7 @@ def generate_estimates(names: list[str], evaluations: set[tuple[str, str]]) -> d
 
     with pm.Model() as model:
         # Define latent variables for the true values of the items
-        name_vars = pm.Normal('names', mu=0, sigma=1, shape=len(names))
+        name_vars = pm.Normal("names", mu=0, sigma=1, shape=len(names))
 
         deltas = 0.5 * pt.tensor.ones_like(mask)
 
@@ -87,21 +92,21 @@ def generate_estimates(names: list[str], evaluations: set[tuple[str, str]]) -> d
         outputs_info = None
         non_sequences = [name_vars]
 
-        deltas_list, _ = pt.scan(fn=compute_delta,
-                                sequences=sequences,
-                                outputs_info=outputs_info,
-                                non_sequences=non_sequences)
+        print(f"{sequences=} {outputs_info=} {non_sequences=}")
+        deltas_list, _ = pt.scan(
+            fn=compute_delta, sequences=sequences, outputs_info=outputs_info, non_sequences=non_sequences
+        )
 
         deltas = set_subtensor(deltas[mask], deltas_list)
 
         p = pm.math.sigmoid(deltas)
         # Fix the likelihood computation to only consider the compared pairs
-        pm.Bernoulli('obs', p=p[mask], observed=observed_data[mask])
+        pm.Bernoulli("obs", p=p[mask], observed=observed_data[mask])
 
         trace = pm.sample(2000)
 
     samples = np.vstack(trace.posterior.names)
-    estimates: dict[str, float] = dict()
+    estimates: dict[str, tuple[float, float, float]] = dict()
     for name in names:
         name_idx = names.index(name)
         estimate = samples[:, name_idx].mean()
@@ -109,31 +114,35 @@ def generate_estimates(names: list[str], evaluations: set[tuple[str, str]]) -> d
         top_k_chance = np.mean([name_idx in list(reversed(sample.argsort()))[:K] for sample in samples])
 
         estimates[name] = (estimate, estimate_std, top_k_chance)
-    
+
     return estimates
 
 
 def write_estimates(conn: Connection, estimates: dict[str, tuple[float, float]]):
-    estimate_tuples = [(name, rating, rating_std, top_chance)
-                       for name, (rating, rating_std, top_chance) in estimates.items()]
+    estimate_tuples = [
+        (name, rating, rating_std, top_chance) for name, (rating, rating_std, top_chance) in estimates.items()
+    ]
 
-    conn.execute('begin')
-    conn.execute('delete from estimates')
-    conn.executemany('''
+    conn.execute("begin")
+    conn.execute("delete from estimates")
+    conn.executemany(
+        """
     insert into estimates (name, rating, rating_std, top_chance) values (?, ?, ?, ?)
-    ''', estimate_tuples)
-    conn.execute('commit')
+    """,
+        estimate_tuples,
+    )
+    conn.execute("commit")
 
 
 if __name__ == "__main__":
     conn = init_db()
     while True:
-        print('Reading evaluations... ', end='')
+        print("Reading evaluations... ", end="")
         names, evaluations = read_evaluations(conn)
-        print('done.\nGenerating estimates... ', end='')
+        print("done.\nGenerating estimates... ", end="")
         estimates = generate_estimates(names, evaluations)
-        print('Writing estimates...', end='')
+        print("Writing estimates...", end="")
         write_estimates(conn, estimates)
-        print('done.\nSleeping.' )
+        print("done.\nSleeping.")
 
         time.sleep(1)
--- ui.py
+++ ui.py
@@ -1,18 +1,23 @@
-import streamlit as st
+import itertools as it
+import random
 import sqlite3
 from sqlite3 import Connection
-import random
-import itertools as it
+
 import numpy as np
+import streamlit as st
+
 from popular_names import show_rank
 
-K = 15 
+K = 15
 DROP_FREQUENCY = 15
 
 st.set_page_config(page_title="Names", page_icon="🥔", layout="wide", initial_sidebar_state="auto", menu_items=None)
 
+
 def names_left_to_rank(conn: Connection) -> list[tuple[str, str]]:
-   return conn.cursor().execute('''
+    return (
+        conn.cursor()
+        .execute("""
    select n1.name, n2.name
    from names n1
    join names n2 on n1.name != n2.name
@@ -20,12 +25,14 @@ def names_left_to_rank(conn: Connection) -> list[tuple[str, str]]:
    left join evaluations e2 on n1.name = e2.loser and n2.name = e2.winner
    where e1.winner is null and e2.loser is null
    order by random()
-   ''').fetchall()
+   """)
+        .fetchall()
+    )
 
 
 def choose_names_to_rank_random(conn: Connection) -> tuple[str, str]:
     name1, name2 = names_left_to_rank(conn)[0]
-    
+
     return random.sample([name1, name2], k=2)
 
 
@@ -37,41 +44,46 @@ def choose_names_to_rank_top(conn: Connection) -> tuple[str, str]:
     temperature = 15
     name_pairs_left = names_left_to_rank(conn)
     top_chances = {name: top_chance for name, _, _, top_chance in get_estimates(conn)}
-    
+
     def score_pair(name1: str, name2: str) -> float:
         return -max(epsilon, abs(top_chances[name1] - 50) + abs(top_chances[name2] - 50))
 
     raw_pair_scores = np.array([score_pair(name1, name2) for name1, name2 in name_pairs_left]) / temperature
     pair_scores = np.exp(raw_pair_scores) / sum(np.exp(raw_pair_scores))
 
+    print(f"{name_pairs_left=}")
     idx = np.random.choice(np.array(range(len(name_pairs_left))), p=pair_scores)
     return name_pairs_left[idx]
 
 
 def insert_evaluation(conn: Connection, winner: str, loser: str):
-    conn.execute('insert into evaluations (winner, loser) values (?, ?)', (winner, loser))
+    conn.execute("insert into evaluations (winner, loser) values (?, ?)", (winner, loser))
 
 
 def drop_evaluation(conn: Connection):
-    conn.execute('delete from evaluations where rowid in (select rowid from evaluations order by random() limit 1);')
+    conn.execute("delete from evaluations where rowid in (select rowid from evaluations order by random() limit 1);")
 
 
 def evaluation_progress(conn: Connection) -> tuple[int, int]:
-    evaluated = conn.cursor().execute('select count(*) from evaluations').fetchone()[0]
-    names = conn.cursor().execute('select name from names').fetchall()
+    evaluated = conn.cursor().execute("select count(*) from evaluations").fetchone()[0]
+    names = conn.cursor().execute("select name from names").fetchall()
 
     return evaluated, len(list(it.combinations(names, r=2)))
 
 
 def get_estimates(conn: Connection) -> list[tuple[str, float, float, float]]:
-    return conn.cursor().execute('''
+    return (
+        conn.cursor()
+        .execute("""
       select name, round(rating, 2), round(rating_std, 2), 100 * top_chance from estimates
-    ''').fetchall()
+    """)
+        .fetchall()
+    )
 
 
 if __name__ == "__main__":
-    conn = sqlite3.connect('names.db', isolation_level=None)
-    
+    conn = sqlite3.connect("names.db", isolation_level=None)
+
     ranker_col, display_col = st.columns([0.5, 0.5])
 
     with ranker_col:
@@ -87,17 +99,17 @@ if __name__ == "__main__":
             drop_evaluation(conn)
 
         left_col, right_col = st.columns(2)
-        with left_col: 
+        with left_col:
             st.button(name1, key="left_button", on_click=lambda: insert_evaluation(conn, name1, name2))
         with right_col:
             st.button(name2, key="right_button", on_click=lambda: insert_evaluation(conn, name2, name1))
 
-        st.text(f'Evaluated {evaluated_count}/{total_count} name pairs')
+        st.text(f"Evaluated {evaluated_count}/{total_count} name pairs")
 
     estimates = sorted(get_estimates(conn), key=lambda x: x[1], reverse=True)
     with display_col:
-        st.header('Current Ranking')
+        st.header("Current Ranking")
         for idx, (name, value, std, top_k_chance) in enumerate(estimates, start=1):
-            estimate_str = f'{value}±{std};'
-            rank = str(idx) + '.'
-            st.text(f'{rank:<3} {name:<10} ({estimate_str:<11} {round(top_k_chance):>2}% chance of being top {K})')
+            estimate_str = f"{value}±{std};"
+            rank = str(idx) + "."
+            st.text(f"{rank:<3} {name:<10} ({estimate_str:<11} {round(top_k_chance):>2}% chance of being top {K})")
